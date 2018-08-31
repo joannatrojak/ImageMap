b4c7f4664ed407acbb44c43e41dc36677a675eb3
joanna <joannatrojak19@gmail.com>
2018-08-31 10:14:56 +0200

    file uploader finished

--- app.py
+++ app.py
@@ -10,8 +10,16 @@ from werkzeug import secure_filename
 app = Flask(__name__)
 
 @app.route('/upload')
-def upload_file():
+def upload_file_template():
    return render_template('upload.html')
+
+@app.route('/uploader', methods = ['GET', 'POST'])
+def upload_file():
+   if request.method == 'POST':
+      f = request.files['file']
+      print(f)
+      f.save(secure_filename(f.filename))
+      return 'file uploaded successfully'
 
 if __name__ == '__main__':
    app.run(debug = True)
